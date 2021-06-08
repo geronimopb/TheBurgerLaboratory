13fb4dced9cd973b12171bf3d67d19e8bc1fd198
geronimopb <47610273+geronimopb@users.noreply.github.com>
2021-06-07 23:44:32 -0500

    Add files via upload

---
+++ PIA_Front/signup.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Burger Lab</title>
+        <meta name="description" content="This is the description">
+        <link rel="shortcut icon" href="favicon-16x16.png" type="image/x-icon">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="styles.css" />
+    </head>
+    <body>
+        <header class="main-header">
+            <nav class="nav main-nav">
+                <ul>
+                    <li><a href="index.html">Inicio</a></li>
+                    <li><a href="store.html">Menú</a></li>
+                    <li><a href="login.html">Inicio de Sesión</a></li>
+                    <li><a href="signup.php">Sign Up</a></li>
+                </ul>
+            </nav>
+            <h1 class="band-name band-name-large">The Burger Laboratory</h1>
+            <div class="container">
+                
+            </div>
+            
+        </header>
+        
+        <footer>
+            <div class="contenedor-footer">
+                <div class="content-foo">
+                    <h4>FaceBook</h4>
+                    <p>Burger Laboratory</p>
+                </div>
+                <div class="content-foo">
+                    <h4>Twitter</h4>
+                    <p>@BurgerLaboratory</p>
+                </div>
+                <div class="content-foo">
+                    <h4>Instagram</h4>
+                    <p>@BurgerLab</p>
+                </div>
+            </div>
+            <h2 class="titulo-final">&copy; Gerónimo Peña Butt | Desarrollo Web</h2>
+        </footer>
+    </body>
+</html>
